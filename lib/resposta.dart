5ae09144abbb421f3b14adea0ad25cf3fb1b8466
Eduardo Mendonca <edusilvamendonca@gmail.com>
2021-05-13 19:44:25 -0400

    Passando a Função Callback

--- lib/resposta.dart
+++ lib/resposta.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
-  final String resposta;
+  final String texto;
+  final void Function() quandoSelecionado;
 
-  Resposta(this.resposta);
+  Resposta(this.texto, this.quandoSelecionado);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(
-        resposta,
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.blue,
+        ),
+        child: Text(
+          texto,
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        onPressed: quandoSelecionado,
       ),
     );
   }
