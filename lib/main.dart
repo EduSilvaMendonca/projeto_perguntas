ade5e7a8a4b15b43ea9091db9889f28b70f92043
Eduardo Mendonca <edusilvamendonca@gmail.com>
2021-05-13 20:20:40 -0400

    Refatorando código para um map de Strings

--- lib/main.dart
+++ lib/main.dart
@@ -31,11 +31,14 @@ class _PerguntaAppState extends State<PerguntaApp> {
       }
     ];
 
-    List<Widget> respostas = [];
+    List<String> respostas = perguntas[_perguntaSelecionada]['respostas'];
+    List<Widget> widgets = respostas
+      .map((t) => Resposta(t, _responder))
+      .toList();
 
-    for(var textoResp in perguntas[_perguntaSelecionada]['respostas']) {
-      respostas.add(Resposta(textoResp, _responder));
-    }
+    // for(var textoResp in respostas) {
+    //   widgets.add(Resposta(textoResp, _responder));
+    // }
 
     return MaterialApp(
       home: Scaffold(
@@ -45,7 +48,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: [
             Questao(perguntas[_perguntaSelecionada]['texto']),
-            ...respostas,
+            ...widgets,
           ],
         ),
       ),
